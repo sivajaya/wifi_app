6b22cbb54a7c7148bf65483bba81c75b8cf79871
siva <sivajaya75@gmail.com>
2015-06-30 18:22:14 +0530

    search

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
+   $("#date").datepicker({dateFormat: "yy-M-dd"}).datepicker("setDate", new Date());
+
   $('#dataTables-example').DataTable({
     responsive: true
   });
 });
-$(function(){
- $("#date").datepicker(
-  {dateFormat: "yy-M-dd"}).datepicker("setDate", new Date());
-});
+
 
